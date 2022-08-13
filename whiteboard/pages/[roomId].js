8dfdc61ebb1ae61cb1adf1cfb486766bee957b67
finbkd <vikas.kumawat66@gmail.com>
2022-08-13 10:25:38 +0530

    add color picker for line and rectangle

--- whiteboard/pages/[roomId].js
+++ whiteboard/pages/[roomId].js
@@ -1,8 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { socketInit } from "../context/socket";
 import styles from "../styles/Home.module.css";
+import { HexColorPicker } from "react-colorful";
 
 const pos = { x: 0, y: 0 };
 const restorePts = { a: 0, b: 0, c: 0, d: 0 };
@@ -14,8 +13,9 @@ export default function Home() {
     socket.current = socketInit();
   }, []);
 
-  const [restorepoints, setrestorePoints] = useState([]);
+  const [color, setColor] = useState("#000");
   const [points, setPoints] = useState([]);
+  const [restorepoints, setrestorePoints] = useState([]);
 
   const [pencilElements, setPencilElements] = useState([]);
 
@@ -26,20 +26,32 @@ export default function Home() {
   const [otherUserElements, setOtherUserElements] = useState([]);
   const [myUserElements, setMyUserElements] = useState([]);
 
+  const [colorPickerOption, setColorPickerOption] = useState(false);
+
   const canvasApi = useRef();
   const contextRef = useRef(null);
 
   //a/ SOCKET IO FOR OTHER USER
   useEffect(() => {
     socket.current.on("lineDrew", (x) => {
-      const { elements, otherUserElements } = x;
+      const { elements, otherUserElements, pencilElements } = x;
 
       setOtherUserElements(elements);
 
       const ctx = canvasApi?.current?.getContext("2d");
       ctx.clearRect(0, 0, 950, 450);
+
       elements.forEach((ele) => actualine(ele.roughEle));
       otherUserElements.forEach((ele) => actualine(ele.roughEle));
+      pencilElements?.forEach((elee) => {
+        elee.forEach((ele, i) => {
+          const newEle = elee[i + 1];
+          contextRef.current.moveTo(ele.clientX, ele.clientY);
+          contextRef.current.lineTo(newEle?.clientX, newEle?.clientY);
+          contextRef.current.stroke();
+          ctx.beginPath();
+        });
+      });
     });
   }, []);
 
@@ -61,7 +73,7 @@ export default function Home() {
 
       elements.forEach((ele) => actualine(ele.roughEle));
       otherUserElements.forEach((ele) => actualine(ele.roughEle));
-      socket.current.emit("drawing", { elements, otherUserElements });
+      socket.current.emit("drawing", { elements, otherUserElements, pencilElements });
     }
 
     if (options === "rectangle") {
@@ -79,24 +91,29 @@ export default function Home() {
 
       elements.forEach((ele) => actualine(ele.roughEle));
       otherUserElements.forEach((ele) => actualine(ele.roughEle));
-      socket.current.emit("drawing", { elements, otherUserElements });
+      socket.current.emit("drawing", { elements, otherUserElements, pencilElements });
     }
-  }, [elements]);
+    if (options === "pencil") {
+      socket.current.emit("drawing", { elements, otherUserElements, pencilElements });
+    }
+  }, [elements, pencilElements]);
 
   //a/ ACTUAl LINE
   const actualine = (x) => {
     if (!x) return;
-    const { options, start1, start2, end1, end2 } = x;
+    const { options, color, start1, start2, end1, end2 } = x;
 
     const ctx = canvasApi.current.getContext("2d");
     if (options === "line") {
       ctx.beginPath();
+      ctx.strokeStyle = color;
       ctx.moveTo(start1, start2);
       ctx.lineTo(end1, end2);
       ctx.stroke();
     }
     if (options === "rectangle") {
       ctx.beginPath();
+      ctx.strokeStyle = color;
       ctx.rect(start1, start2, end1 - start1, end2 - start2);
       ctx.stroke();
     }
@@ -106,11 +123,9 @@ export default function Home() {
   useEffect(() => {
     const ctx = canvasApi.current.getContext("2d");
     if (options === "pencil") {
-      ctx.lineCap = "round";
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 2;
       contextRef.current = ctx;
 
+      contextRef.current.strokeStyle = color;
       points.forEach((ele) => {
         contextRef.current.lineTo(ele.x, ele.y);
         contextRef.current.stroke();
@@ -118,10 +133,11 @@ export default function Home() {
     }
   }, [points]);
 
-  const drawShape = (options, start1, start2, end1, end2) => {
+  const drawShape = (options, color, start1, start2, end1, end2) => {
     const ctx = canvasApi.current.getContext("2d");
-    const roughEle = { options, start1, start2, end1, end2 };
+    const roughEle = { options, color, start1, start2, end1, end2 };
     if (options === "line") {
+      ctx.strokeStyle = color;
       ctx.beginPath();
       ctx.moveTo(start1, start2);
       ctx.lineTo(end1, end2);
@@ -132,7 +148,7 @@ export default function Home() {
       ctx.rect(start1, start2, end1 - start1, end2 - start2);
       ctx.stroke();
     }
-    return { options, start1, start2, end1, end2, roughEle };
+    return { options, color, start1, start2, end1, end2, roughEle };
   };
 
   //a/ DRAWING
@@ -142,8 +158,8 @@ export default function Home() {
       const index = elements.length - 1;
       if (options === "line") {
         const { clientX, clientY } = event;
-        const { options, start1, start2 } = elements[index];
-        const updatedEle = drawShape("line", start1, start2, clientX, clientY);
+        const { options, color, start1, start2 } = elements[index];
+        const updatedEle = drawShape("line", color, start1, start2, clientX, clientY);
         const copyElement = [...elements];
         copyElement[index] = updatedEle; //replacing last index
         setElements(copyElement);
@@ -151,15 +167,18 @@ export default function Home() {
       }
       if (options === "rectangle") {
         const { clientX, clientY } = event;
-        const { options, start1, start2 } = elements[index];
-        const updatedEle = drawShape("rectangle", start1, start2, clientX, clientY);
+        const { options, color, start1, start2 } = elements[index];
+        const updatedEle = drawShape("rectangle", color, start1, start2, clientX, clientY);
         const copyElement = [...elements];
         copyElement[index] = updatedEle; //replacing last index
         setElements(copyElement);
       }
       if (options === "pencil") {
         contextRef.current = ctx;
+
         setPoints((state) => [...state, pos]);
+
+        contextRef.current.strokeStyle = color;
         contextRef.current.moveTo(pos.x, pos.y);
 
         setrestorePoints((state) => [...state, { clientX, clientY }]);
@@ -178,12 +197,12 @@ export default function Home() {
   const startDrawing = (e) => {
     if (options === "line") {
       setDrawing(true);
-      const newEle = drawShape("line", e.clientX, e.clientY, e.clientX, e.clientY);
+      const newEle = drawShape("line", color, e.clientX, e.clientY, e.clientX, e.clientY);
       setElements((state) => [...state, newEle]);
     }
     if (options === "rectangle") {
       setDrawing(true);
-      const newEle = drawShape("rectangle", e.clientX, e.clientY, e.clientX, e.clientY);
+      const newEle = drawShape("rectangle", color, e.clientX, e.clientY, e.clientX, e.clientY);
       setElements((state) => [...state, newEle]);
     }
     if (options === "pencil") {
@@ -201,24 +220,38 @@ export default function Home() {
     setDrawing(false);
     setPencilElements((state) => [...state, restorepoints]);
     setrestorePoints([]);
+    setPoints([]);
   };
 
   const changeMode = (x) => {
     setOptions(x);
   };
 
+  const colorPicker = () => {
+    setColorPickerOption(!colorPickerOption);
+  };
+
   return (
     <div className={styles.container}>
-      <>
-        <div>Switch To</div>
+      <div className={styles.canvasContainer}>
+        <canvas className={styles.canvas} id="tutorial" ref={canvasApi} onMouseDown={startDrawing} onMouseUp={finishDrawing} onMouseMove={draw}></canvas>
         <div className={styles.options}>
-          <span onClick={() => changeMode("line")}>Line</span>
-          <span onClick={() => changeMode("rectangle")}>Rectangle</span>
-          <span onClick={() => changeMode("pencil")}>Pencil</span>
-          <div className={styles.mode}>{options}</div>
+          <div onClick={() => changeMode("line")} className={styles.option}>
+            <img className={options === "line" ? `${styles.optionIcon}   ${styles.active}` : `${styles.optionIcon}`} src="/icons/line.png" />
+          </div>
+          <div onClick={() => changeMode("rectangle")} className={styles.option}>
+            <img className={options === "rectangle" ? `${styles.optionIcon}   ${styles.active}` : `${styles.optionIcon}`} src="/icons/rectangle.png" />
+          </div>
+          <div onClick={() => changeMode("pencil")} className={styles.option}>
+            <img className={options === "pencil" ? `${styles.optionIcon}   ${styles.active}` : `${styles.optionIcon}`} src="/icons/pencil.png" />
+          </div>
+          <div className={styles.option}>
+            <img onClick={colorPicker} className={options === "colorr" ? `${styles.optionIcon}   ${styles.active}` : `${styles.optionIcon}`} src="/icons/color-dropper.png" />
+          </div>
+          {colorPickerOption && <HexColorPicker color={color} onChange={setColor} />}
         </div>
-      </>
-      <canvas className={styles.canvas} id="tutorial" width="950" height="450" ref={canvasApi} onMouseDown={startDrawing} onMouseUp={finishDrawing} onMouseMove={draw}></canvas>
+      </div>
+      <div className={styles.chatBox}>CHAT BOX</div>
     </div>
   );
 }
